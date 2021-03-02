906f0e15b6101027231ed7c35632b194030654b3
pfpimenta <pedrofolettopimenta@gmail.com>
2021-03-02 19:20:37 -0300

    main python files

---
+++ src/classify.py
@@ -0,0 +1,18 @@
+# TODO description
+
+
+
+def classify_sample(sample) -> bool:
+    """
+        TODO pydoc
+        classifies one sample
+    """
+    # TODO
+
+
+def classify_batch(batch) -> list:
+    """
+        TODO pydoc
+        classifies one batch of samples
+    """
+    # TODO
---
+++ src/web_api.py
@@ -0,0 +1,15 @@
+# This script contains the endpoints for the WebAPI
+
+
+from flask import flask
+
+app = Flask(__name__)
+
+# endpoint to train the model
+# TODO
+
+# endpoint to classify a single sample
+# TODO
+
+# endpoint to classify a batch of samples
+# TODO
