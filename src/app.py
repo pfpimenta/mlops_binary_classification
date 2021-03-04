1c8fef4e448181b044994bbdc0f41d06e6ffde0d
pfpimenta <pedrofolettopimenta@gmail.com>
2021-03-04 11:52:55 -0300

    training simple random forest model

--- src/app.py
+++ src/app.py
@@ -1,40 +1,50 @@
 # -*- coding: utf-8 -*-
 # This script contains the endpoints for the WebAPI
 
-import logging
-
 from flask import Flask
 
+from classify import classify_batch, classify_sample
 from healthcheck import check_health
-from train import train_model
-from classify import classify_sample, classify_batch
+from train import train
 
 app = Flask(__name__)
 
+# hello world checkpoint (to test if API is up)
+@app.route("/")
+def hello_world():
+    app.logger.info("Processing default request")
+    return "Hello World!"
+
+
 # healthcheck endpoint
-# TODO
 @app.route("/healthcheck", methods=["GET"])
 def endpoint_healthcheck():
     health = check_health()
-    logging("Current system health: " + str(health))
-    return health, 200
+    app.logger.info("Current system health: ", str(health))
+    return health
 
 
 # endpoint to train the model
 # TODO
-@app.route("/classify_sample", methods=["GET"])
+@app.route("/train", methods=["GET"])
 def endpoint_train():
-    logging.debug("Training the model...")
-    train_model()
-    return "Model has successfully trained", 200
+    app.logger.info("Training the model...")
+    # TODO refactor
+    model, score = train()  # TODO save model state somehow
+    return (
+        f"Model has been trained successfully. Score on the test data: {score}\n",
+        200,
+    )
+
 
 # endpoint to classify a single sample
 # TODO
 @app.route("/classify_sample", methods=["GET"])
 def endpoint_classify_sample(sample):
-    logging.debug("Classifying sample: %s", str(sample))
-    # TODO : validate sample data
-    return "The given ", 200
+    # logging.debug("Classifying sample: %s", str(sample))
+    predicted_class = classify_sample(sample)
+    # TODO log readable string with the result
+    return predicted_class, 200
 
 
 # endpoint to classify a batch of samples
@@ -42,9 +52,10 @@ def endpoint_classify_sample(sample):
 @app.route("/classify_batch", methods=["GET"])
 def endpoint_classify_batch(batch: list) -> list:
     num_samples = len(batch)
-    logging.debug("Classifying batch with %s samples", num_samples)
-    pass
+    # logging.debug("Classifying batch with %s samples", num_samples)
+    predicted_batch_classes = classify_batch(batch)
+    return predicted_batch_classes
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9200)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=9200)
--- src/train.py
+++ src/train.py
@@ -1,2 +1,24 @@
 # -*- coding: utf-8 -*-
-# TODO
+
+from sklearn.ensemble import RandomForestClassifier
+
+from data_loader import get_test_data, get_training_data
+
+
+def train_model():
+    train_X, train_Y = get_training_data()
+    model = RandomForestClassifier(random_state=0)
+    model.fit(train_X, train_Y)
+    return model
+
+
+def evaluate_model(model) -> float:
+    test_X, test_Y = get_testdata()
+    score = model.score(test_X, test_Y)
+    return score
+
+
+def train():
+    model = train_model()
+    score = evaluate_model(model)
+    return model, score
